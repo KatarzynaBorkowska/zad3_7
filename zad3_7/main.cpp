56ec6d5825d5385077fdb9ce883b8c187e5adee1
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-05-14 10:09:28 +0200

    Initial Commit

---
+++ zad3_7/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  zad3_7
+//
+//  Created by Kasia on 14/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
